4d2797f7ebf92cf9dd680a4f5b125ed7f16127a3
rasputinforever <erik.justin.portillo@gmail.com>
2021-04-14 19:59:43 -0700

    moved searh form functions to component

--- client/src/components/BookSearch.js
+++ client/src/components/BookSearch.js
@@ -2,10 +2,17 @@ import React from 'react';
 
 import { Button, Form } from 'semantic-ui-react'
 
-function BookSearch({ onSearch }){
+import Book from './Book.js'
+
+import API from '../utils/API.js'
+
+function BookSearch({ userid, onNewBook }){
     // form states
     const [bookTitle, setBookTitle] = React.useState('')
     const [bookAuthor, setBookAuthor] = React.useState('')
+
+    const [searchFail, setSearchFail] = React.useState(false)
+    const [searchResults, setSearchResults] = React.useState([])
 
     function handleInputChange(e) {
         switch(e.target.name) {
@@ -20,9 +27,52 @@ function BookSearch({ onSearch }){
           }     
     }
 
-    function handleSubmit() {
-        onSearch(bookTitle, bookAuthor)
+    function handleSubmit(){
+        
+        const queryString = ((bookTitle) ? bookTitle : '') + ((bookTitle && bookAuthor) ? ' ' : '') + ((bookAuthor) ? bookAuthor : '')
+        
+        API.googleBook(queryString)
+        
+        .then((data) => {
+            let newResults = []
+            if (data.data.items) {
+                
+                setSearchFail(false)
+
+                data.data.items.forEach((book) => {
+                    newResults.push({
+                        id: book.id,
+                        title: book.volumeInfo.title,
+                        authors: book.volumeInfo.authors,
+                        description: book.volumeInfo.description,
+                        thumb: book.volumeInfo.imageLinks.smallThumbnail,
+                        img: book.volumeInfo.imageLinks.thumbnail,
+                    })
+                })
+            } else {
+                setSearchFail(true)
+            }
+            
+            // worry about dupes? 
+            setSearchResults(newResults)
+            
+        });
     }
+
+    function newBookRequest(newBook){
+        // sends book object, with ID, to db
+        API.addBook(userid, newBook)
+        .then(() => {
+            setSearchResults([])
+
+            onNewBook(userid)
+        })
+    }
+
+    // rendered search results
+    const foundBooks = searchResults.map((book, i) => {
+        return <Book key={i} id={book.id} title={book.title} authors={book.authors} description={book.description} thumb={book.thumb} onSubmit={newBookRequest} />
+    })
 
     return (
         <>
@@ -49,6 +99,9 @@ function BookSearch({ onSearch }){
                 <Button type='submit' onClick={handleSubmit}>Submit</Button>
 
             </Form>
+            
+            {searchFail ? <h2>Nothing Found!</h2> : foundBooks}
+
         </>
     )
 }
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -24,79 +24,15 @@ function Home({ user, userid }){
         })
     }
 
-    // Searching states
-    const [isSearching, setIsSearching] = React.useState(false)
-    const [searchResults, setSearchResults] = React.useState([])
-    const [searchFail, setSearchFail] = React.useState(false)
-
-    // search button
-    function handleNewSearch() {
-        setIsSearching(true)
-    }
-
-    // API call to google
-    function handleSubmit(bookTitle, bookAuthor){
-        
-        const queryString = ((bookTitle) ? bookTitle : '') + ((bookTitle && bookAuthor) ? ' ' : '') + ((bookAuthor) ? bookAuthor : '')
-        
-        API.googleBook(queryString)
-        
-        .then((data) => {
-            let newResults = []
-            if (data.data.items) {
-                
-                setSearchFail(false)
-
-                data.data.items.forEach((book) => {
-                    newResults.push({
-                        id: book.id,
-                        title: book.volumeInfo.title,
-                        authors: book.volumeInfo.authors,
-                        description: book.volumeInfo.description,
-                        thumb: book.volumeInfo.imageLinks.smallThumbnail,
-                        img: book.volumeInfo.imageLinks.thumbnail,
-                    })
-                })
-            } else {
-                setSearchFail(true)
-            }
-            
-            // worry about dupes? 
-            setSearchResults(newResults)
-            
-        });
-    }
-
-    function newBookRequest(newBook){
-        // sends book object, with ID, to db
-        API.addBook(userid, newBook)
-        .then(() => {
-            setSearchResults([])     
-            setIsSearching(false)
-
-            searchBooks(userid)
-        })
-    }
-
-    // rendered search results
-    const foundBooks = searchResults.map((book, i) => {
-        return <Book key={i} id={book.id} title={book.title} authors={book.authors} description={book.description} thumb={book.thumb} onSubmit={newBookRequest} />
-    })
-
-    // renderee bookshelf
-    const bookShelf = <Shelf userid={userid} data={shelfData} onDelete={searchBooks} /> 
-
     return (
         <>
             <h1>This will be a Title, {user}!</h1>
             <h1>This will be a button that kills the user!</h1>
 
-            {bookShelf}
+            <Shelf userid={userid} data={shelfData} onDelete={searchBooks} /> 
 
-            {isSearching ? <BookSearch onSearch={handleSubmit} /> : <button onClick={handleNewSearch}>New Search</button> }
+            <BookSearch userid={userid} onNewBook={searchBooks} />
 
-            {searchFail ? <h2>Nothing Found!</h2> : <></>}
-            {isSearching ? foundBooks : <></>}
         </>
     )
 }
